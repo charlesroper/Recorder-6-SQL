4840930e56c573ed774a211273b2a5248933b2ed
Charles Roper <charles@roper.im>
2015-02-27 10:16:20 +0000

    Add comment about the 27700 EPSG code.

--- NBN Exports/NBNexport_020_add_geographic_points.sql
+++ NBN Exports/NBNexport_020_add_geographic_points.sql
@@ -3,6 +3,8 @@ IF OBJECT_ID('NBNReporting.dbo.ALL_NBN_geo', 'U') IS NOT NULL
   DROP TABLE NBNReporting.dbo.ALL_NBN_geo; 
 
 -- Add geometry points and create new table
+-- Note: "27700" is the EPSG code for the OSGB1936 spatial reference system
+-- See here: http://spatialreference.org/ref/epsg/osgb-1936-british-national-grid/
 select
   *
   ,Geo = geometry::Point(X, Y, 27700)
